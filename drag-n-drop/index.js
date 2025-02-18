7632de56e7a7999beb19ed734a6926133b5fbe7a
sprezza <paulanselmendoza@gmail.com>
2025-02-18 21:20:01 +0800

    DragNDrop: Initial Code

---
+++ drag-n-drop/index.js
@@ -0,0 +1,21 @@
+const draggableElement = document.querySelectorAll(".draggable");
+const droppableElement = document.querySelectorAll(".droppable");
+
+/*  Draggable item events
+    - dragstart
+    - drag
+    - dragend
+*/
+draggableElement.forEach((element) => {
+    element.addEventListener();
+});
+
+/*  Droppable item events
+    - dragenter
+    - dragover
+    - dragleave
+    - drop
+*/
+draggableElement.forEach((element) => {
+    element.addEventListener();
+});
